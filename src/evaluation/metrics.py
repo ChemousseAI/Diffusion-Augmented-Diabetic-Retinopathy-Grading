462e21b970da824f13b4d7f805e9bfbe7c33b55e
Chemousse Berdjouh <alaholadanqatar@gmail.com>
2026-06-17 16:44:17 +0100

    Add files via upload

---
+++ src/evaluation/metrics.py
@@ -0,0 +1,14 @@
+
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    f1_score,
+)
+
+__all__ = [
+    "accuracy_score",
+    "classification_report",
+    "confusion_matrix",
+    "f1_score",
+]
